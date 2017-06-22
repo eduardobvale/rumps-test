7f79793ca999ca7a7c4bd0da5f5b279e2b84227b
Eduardo Vale <eduardo@vistoapp.com>
2017-06-22 13:36:26 -0300

    Translating

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import rumps
 import os
 
 class AwesomeStatusBarApp(rumps.App):
-    @rumps.clicked("Limpar Lixeira")
+    @rumps.clicked("Clear Trash")
     def prefs(self, _):
         os.system("sudo rm -rf ~/.Trash/*")
 
-    @rumps.clicked("Resetar Áudio")
+    @rumps.clicked("Reset Audio")
     def prefs(self, _):
         os.system("sudo kill -9 `ps ax|grep 'coreaudio[a-z]' |awk '{print $1}'`")
 
